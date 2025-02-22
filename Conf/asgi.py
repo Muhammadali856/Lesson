f6e1f27e481176892569248a5e263db5927d6c90
Muhammadali856 <eshboevmuhammadali2@gmail.com>
2025-02-22 19:41:07 +0500

    Proyekt nomi o'zgartirildi

--- Conf/asgi.py
+++ Conf/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MEDIUM project.
+ASGI config for Conf project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MEDIUM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Conf.settings')
 
 application = get_asgi_application()
