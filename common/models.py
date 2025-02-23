bd94c06e5e8c35d0d7401479ba722419bf7adcc9
Muhammadali856 <eshboevmuhammadali2@gmail.com>
2025-02-23 19:23:40 +0500

    Model added

--- common/models.py
+++ common/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class UserModel(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.PositiveSmallIntegerField()
+
+
+class ProfileModel(models.Model):
+    user = models.OneToOneField(UserModel, on_delete=models.PROTECT)
+
+
+class BlogModel(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+class CategoriesModel(models.Model):
+    title = models.CharField(max_length=255)
+
+class ProductModel(models.Model):
+    categories = models.ManyToManyField(CategoriesModel)
