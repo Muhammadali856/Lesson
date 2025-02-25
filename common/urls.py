e75417631242a8522f5559536769e2e296716653
Muhammadali856 <eshboevmuhammadali2@gmail.com>
2025-02-25 21:28:27 +0500

    templates_managed_3

--- common/urls.py
+++ common/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('author/', views.author_page_views, name= 'author'),
     path('contact/', views.contact_page_views, name= 'contact'),
     path('single_author/', views.single_author_page_views, name= 'single_author'),
+    path('category/', views.category_page_views, name= 'category'),
+    path('blog/', views.blog_page_views, name= 'blog'),
+    path('recipe/', views.recipe_page_views, name= 'recipe'),
+    path('shop/', views.shop_page_views, name= 'shop'),
 ]
--- common/views.py
+++ common/views.py
@@ -14,3 +14,15 @@ def contact_page_views(request):
 
 def single_author_page_views(request):
     return render(request, template_name='pages/single_author.html')
+
+def category_page_views(request):
+    return render(request, template_name='recipe/category.html')
+
+def blog_page_views(request):
+    return render(request, template_name='blog/blog_list.html')
+
+def recipe_page_views(request):
+    return render(request, template_name='recipe/recipe_with_sidebar.html')
+
+def shop_page_views(request):
+    return render(request, template_name='shop/shop.html')
